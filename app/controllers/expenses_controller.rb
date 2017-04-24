def8fd97f40e44aab457dd27450d00c7924deaab
Javier Vargas <javier1615@gmail.com>
2017-04-24 14:59:51 -0500

    device

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,7 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
   def index
-     @expenses = Expense.order("date DESC")
+     @expenses = User.find(current_user.id).expenses
      if params[:concept].present? && !params[:category_id].present?
      	@expenses = @expenses.where("concept LIKE :concept", concept: "%#{params[:concept]}%")
      	elsif params[:concept].present? && params[:category_id].present?
