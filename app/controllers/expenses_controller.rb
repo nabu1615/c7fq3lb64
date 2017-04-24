e0c14958a0bdbad46f5ecfa0fb08a83b452ade15
Javier Vargas <javier1615@gmail.com>
2017-04-24 10:30:38 -0500

    add-filter-behavior-for-index-controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,10 @@
 class ExpensesController < ApplicationController
   def index
      @expenses = Expense.order("date DESC")
+     if params[:concept].present?
+     	@expenses = @expenses.where("concept LIKE :concept", concept: "%#{params[:concept]}%")
+     	elsif params[:category_id].present?
+     		@expenses = @expenses.where("category_id = #{params[:category_id]}")
+     end
   end
 end
