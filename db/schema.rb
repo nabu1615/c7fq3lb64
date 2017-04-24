a7e17baa2b2c2fc7e860c7e8c8228638fdbdb968
Javier Vargas <javier1615@gmail.com>
2017-04-24 15:35:10 -0500

    adding-log-out-button

--- db/schema.rb
+++ db/schema.rb
@@ -23,9 +23,11 @@ ActiveRecord::Schema.define(version: 20170424165855) do
     t.date     "date"
     t.string   "concept"
     t.decimal  "amount"
+    t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["category_id"], name: "index_expenses_on_category_id"
+    t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
